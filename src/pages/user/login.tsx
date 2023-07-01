2828be3334bc3cc8efc656d5d4cc4dc1e17a222c
henry <henry@yescnc.co.kr>
2023-07-02 00:40:08 +0900

    clean the code in reducers

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,11 +1,11 @@
 // project import
-import FirebaseSocial from '../user/auth-forms/FirebaseSocial';
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { BaseUrl } from '../../util/axiosApi';
-import axios from 'axios';
-import { set } from '../../reducers/userReducer'
+import FirebaseSocial from '../user/auth-forms/FirebaseSocial'
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { BaseUrl } from '../../util/axiosApi'
+import axios from 'axios'
+import { set } from '../../reducers/user/userReducer'
 
 // material-ui
 import {
@@ -21,74 +21,85 @@ import {
   Stack,
   Typography,
   Box
-} from '@mui/material';
+} from '@mui/material'
 
 const Login: React.FC = () => {
+  const navigate = useNavigate()
 
-    const navigate = useNavigate();
+  const goHome = () => {
+    navigate('/')
+  }
+  const goJoin = () => {
+    navigate('/join')
+  }
 
-    const goHome = () => {
-      navigate('/')
-    };
-    const goJoin = () => {
-      navigate('/join')
-    };
+  const goPwsearch = () => {
+    navigate('/pwsearch')
+  }
 
-    const goPwsearch = () => {
-      navigate('/pwsearch')
-    };
+  const [email, setEmailValue] = React.useState('')
+  const emailChange = (newValue: string) => {
+    setEmailValue(newValue)
+  }
 
-    const [email, setEmailValue] = React.useState('');
-    const emailChange = (newValue: string) => {
-        setEmailValue(newValue);
-    };
+  const [pw, setPwValue] = React.useState('')
+  const pwChange = (newValue: string) => {
+    setPwValue(newValue)
+  }
+  const dispatch = useDispatch()
+  const [checked, setChecked] = React.useState(false)
 
-    const [pw, setPwValue] = React.useState('');
-    const pwChange = (newValue: string) => {
-        setPwValue(newValue);
-    };
-    const dispatch = useDispatch();
-    const [checked, setChecked] = React.useState(false);
-
-    const login = () => {
-      const url = BaseUrl + "/user/login/normal/"
-      axios.post(url, {
-          headers: 
-          {
-              "Content-Type": "application/json"
-          },
-          body: { id: email, pw: pw }
+  const login = () => {
+    const url = BaseUrl + '/user/login/normal/'
+    axios
+      .post(url, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: { id: email, pw: pw }
       })
-      .then(function(response) {
-          const {accessToken} = response.data.access_token;
-          axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-          goHome()
+      .then(function (response) {
+        const { accessToken } = response.data.access_token
+        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+        goHome()
       })
-      .catch(function(error) {
-          alert('로그인정보를 확인해 주세요')
+      .catch(function (error) {
+        alert('로그인정보를 확인해 주세요')
       })
-  };
+  }
 
-    const [showPassword, setShowPassword] = React.useState(false);
-    const handleClickShowPassword = () => {
-      setShowPassword(!showPassword);
-    };
-  
-    const handleMouseDownPassword = (event: { preventDefault: () => void; }) => {
-      event.preventDefault();
-    };
+  const [showPassword, setShowPassword] = React.useState(false)
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
 
-   return (
-        <div className='login'>
-          <Box sx={{marginLeft: 5}}>
-          <Grid container spacing={3}>
-           <Grid item xs={12}>
-            <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
-             <Typography variant="h3">로그인</Typography>
-             <Typography onClick={() => { goJoin() }} variant="body1" sx={{ textDecoration: 'none' }} color="primary">
-              아직 회윈이 아니신가요?
-             </Typography>
-           </Stack>
+  const handleMouseDownPassword = (event: { preventDefault: () => void }) => {
+    event.preventDefault()
+  }
+
+  return (
+    <div className="login">
+      <Box sx={{ marginLeft: 5 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="baseline"
+              sx={{ mb: { xs: -0.5, sm: 0.5 } }}
+            >
+              <Typography variant="h3">로그인</Typography>
+              <Typography
+                onClick={() => {
+                  goJoin()
+                }}
+                variant="body1"
+                sx={{ textDecoration: 'none' }}
+                color="primary"
+              >
+                아직 회윈이 아니신가요?
+              </Typography>
+            </Stack>
           </Grid>
           <Grid item xs={12}>
             <Grid container spacing={3}>
@@ -108,7 +119,7 @@ const Login: React.FC = () => {
                   <OutlinedInput
                     fullWidth
                     type={showPassword ? 'text' : 'password'}
-                    onChange={(newValue) => pwChange(newValue.target.value)} 
+                    onChange={(newValue) => pwChange(newValue.target.value)}
                     endAdornment={
                       <InputAdornment position="end">
                         <IconButton
@@ -117,8 +128,7 @@ const Login: React.FC = () => {
                           onMouseDown={handleMouseDownPassword}
                           edge="end"
                           size="large"
-                        >
-                        </IconButton>
+                        ></IconButton>
                       </InputAdornment>
                     }
                     placeholder="Enter password"
@@ -127,7 +137,12 @@ const Login: React.FC = () => {
               </Grid>
 
               <Grid item xs={12} sx={{ mt: -1 }}>
-                <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  spacing={2}
+                >
                   <FormControlLabel
                     control={
                       <Checkbox
@@ -138,17 +153,32 @@ const Login: React.FC = () => {
                         size="small"
                       />
                     }
-                    label={<Typography variant="h6">Keep me sign in</Typography>}
+                    label={
+                      <Typography variant="h6">Keep me sign in</Typography>
+                    }
                   />
-                  <Typography variant="h6" onClick={() => { goPwsearch() }} color="text.primary">
+                  <Typography
+                    variant="h6"
+                    onClick={() => {
+                      goPwsearch()
+                    }}
+                    color="text.primary"
+                  >
                     Forgot Password?
                   </Typography>
                 </Stack>
               </Grid>
               <Grid item xs={12}>
-                  <Button onClick={(event) => login()} disableElevation fullWidth size="large"  variant="contained" color="primary">
-                    Login
-                  </Button>
+                <Button
+                  onClick={(event) => login()}
+                  disableElevation
+                  fullWidth
+                  size="large"
+                  variant="contained"
+                  color="primary"
+                >
+                  Login
+                </Button>
               </Grid>
               <Grid item xs={12}>
                 <Divider>
@@ -160,11 +190,10 @@ const Login: React.FC = () => {
               </Grid>
             </Grid>
           </Grid>
-         </Grid>
-       </Box>
-      </div>
-    );
+        </Grid>
+      </Box>
+    </div>
+  )
 }
 
-export default Login;
-
+export default Login
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,27 +1,24 @@
-import { combineReducers } from "redux";
-import headerReducer from "./headerReducer";
-import modalReducer from "./modalReducer";
-import userReducer from "./userReducer";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-
+import { combineReducers } from 'redux'
+import headerReducer from './header/headerReducer'
+import modalReducer from './modal/modalReducer'
+import userReducer from './user/userReducer'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const rootReducer = combineReducers({
-    headerReducer,
-    userReducer,
-    modalReducer
+  headerReducer,
+  userReducer,
+  modalReducer
 })
 
 const persistConfig = {
-    key: "root",
-    // localStorage에 저장합니다.
-    storage,
-    // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
-    whitelist: ["userReducer"]
-    // blacklist -> 그것만 제외합니다
-  };
+  key: 'root',
+  // localStorage에 저장합니다.
+  storage,
+  // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
+  whitelist: ['userReducer']
+  // blacklist -> 그것만 제외합니다
+}
 
-export default persistReducer(persistConfig, rootReducer);
+export default persistReducer(persistConfig, rootReducer)
 export type RootState = ReturnType<typeof rootReducer>
-
