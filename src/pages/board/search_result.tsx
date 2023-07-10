088f6f1ab08962c3070734cd1d0e7fe98bc7c15d
sehun910124@naver.com <kimsehun@sehunui-noteubug.local>
2023-07-10 21:55:23 +0900

    restaurant_update,review_write

---
+++ src/pages/board/search_result.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+
+interface ItemType {
+  id: number;
+  food: string;
+}
+
+const Search_result: React.FC = () => {
+
+
+  return (
+    <div>
+      <h1>검색결과 :</h1>
+    </div>
+  );
+}
+
+export default Search_result;
--- src/pages/user/review_write.tsx
+++ src/pages/user/review_write.tsx
@@ -1,7 +1,25 @@
+import '../../assets/styles/css/review_write.css'
+import { useState } from 'react';
+import Choose_star from '../../components/choose_star';
 const review_write: React.FC = () => {
+
     return (
-        <div>
-            <p>리뷰 작성 페이지</p>
+        <div className='review_write'>
+            <div className='review_title'>
+                <strong>식당</strong>에 대한 솔직한 리뷰를 써주세요.
+            </div>
+            <Choose_star/>
+            <textarea></textarea>
+            <p>0/500</p>
+            <input className='file_upload' type='file' accept='image/*'/>
+            <div className='location_section'>
+                <div>위치인증</div>
+                <button  type='button' className='location_section_btn'></button>
+            </div>
+            <hr/>
+            <div className='button_wrap'>
+                <button className='close_btn'>취소</button><button className='upload_btn'>작성</button>
+            </div>
         </div>
     );
 }
