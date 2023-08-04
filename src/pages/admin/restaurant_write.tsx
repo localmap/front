bd93801d34a991601cdbd8326335999a1f2bcf81
ksh <sehun910124@naver.com>
2023-08-04 10:23:18 +0900

    restaurant_write 수정중

--- src/pages/admin/restaurant_write.tsx
+++ src/pages/admin/restaurant_write.tsx
@@ -1,61 +1,62 @@
-import '../../assets/styles/css/admin/restaurant_write.css'
-import { useState } from 'react';
-import Location_search from '../user/location_search';
-import Choose_food from '../../components/choose_food';
-import Choose_price from '../../components/choose_price';
+import "../../assets/styles/css/admin/restaurant_write.css";
+import { useState } from "react";
+import Location_search from "../user/location_search";
+import Choose_food from "../../components/choose_food";
+import Choose_price from "../../components/choose_price";
 
-const Restaurant_write:React.FC = () => {
-    const [visible, setVisible] = useState(false);
+const Restaurant_write: React.FC = () => {
+  const [visible, setVisible] = useState(false);
 
-    return(
-        <div className="restaurant_write">
-            <div className='restaurant_title'>
-                식당이름
-            </div>
-            <input className='restaurant_name' placeholder='상호를 입력해 주세요'></input>
-            <div className='location_select'>
-                <div className='inner'>
-                    <div>지역 및 위치 선택</div>
-                    <button className='location_btn' onClick={()=>setVisible(!visible)} type='button'>
-                    </button>
-                    {visible && (
-                        <div className='drop_down'>
-                            <Location_search/>
-                        </div>
-                    )}
-                </div>
-            </div>
-            <hr/>
-            <div className='food_type'>
-                음식종류
-            </div>
-            <Choose_food/>
-                <hr/>
-            <Choose_price/>
-            <hr/>
-            <div className='menu'>
-                <div className='menu_title'>
-                    <div>메뉴</div>
-                    <button>+</button>
-                </div>
-                <div className='menu_list'>
-                    <div className='menu_img'></div>
-                    <div className='menu_sub'>
-                        <div>오렌지</div>
-                        <div>5000원</div>
-                    </div>
-                </div>
-            </div>
-            <hr/>
-            <div className='hours_title'>
-                영업시간
-            </div>
-            <input className='hours_input' placeholder='영업 시간을 입력해 주세요'></input>
-            <div className='button_wrap'>
-                <button className='cancel_btn'>취소</button><button className='check_btn'>확인</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <main className="restaurant_write">
+      <section className="restaurant_title">
+        <p>식당이름</p>
+        <input
+          className="restaurant_name"
+          placeholder="상호를 입력해 주세요"
+        ></input>
+      </section>
+      <section className="location_select">
+        <span>지역 및 위치 선택</span>
+        <button onClick={() => setVisible(!visible)} type="button"></button>
+        {visible && (
+          <div className="drop_down">
+            <Location_search />
+          </div>
+        )}
+      </section>
+      <section className="food_type">
+        <Choose_food />
+      </section>
+      <hr />
+      <section className="price_type">
+        <Choose_price />
+      </section>
+      <hr />
+      <section className="menu">
+        <section className="menu_title">
+          <span>메뉴</span>
+          <button>+</button>
+        </section>
+        <section className="menu_list">
+          <div className="menu_img"></div>
+          <div className="menu_sub">
+            <span>오렌지</span>
+            <span>5000원</span>
+          </div>
+        </section>
+      </section>
+      <hr />
+      <section className="hours_title">
+        <p>영업시간</p>
+        <input placeholder="영업 시간을 입력해 주세요"></input>
+      </section>
+      <section className="button_wrap">
+        <button className="cancel_btn">취소</button>
+        <button className="check_btn">확인</button>
+      </section>
+    </main>
+  );
+};
 
 export default Restaurant_write;
