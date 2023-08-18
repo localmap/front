6d28f39526b5fdb2b4bbd7d89c01c345d68a94d3
ksh <sehun910124@naver.com>
2023-08-18 19:10:41 +0900

    user_update페이지 모달창

--- src/pages/modal/user_resign.tsx
+++ src/pages/modal/user_resign.tsx
@@ -1,16 +1,31 @@
 import "../../assets/styles/css/modal/user_resign.scss";
-
+import { useDispatch } from "react-redux";
+import { set } from "../../reducers/modal/modalReducer";
+import { RootState } from "../../reducers";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 const User_resign: React.FC = () => {
+  const dispatch = useDispatch();
+  const modalState = useSelector((state: RootState) => state.modalReducer);
+
+  const closeModal = () => {
+    dispatch(set({ ...modalState, state: "" }));
+  };
+
+  if (modalState.state === "") return null;
   return (
-    <div className="user_resign">
-      <header className="user_resign_tit">
-        <span>회원탈퇴</span>
-        <p>~ 님 탈퇴 하시겠습니까?</p>
-      </header>
-      <section className="resign_btn_wraps">
-        <button className="u_resign_btn">탈퇴</button>
-        <button className="u_cancle_btn">취소</button>
-      </section>
+    <div className="modal">
+      <div className="user_resign">
+        <header className="user_resign_tit">
+          <span>회원탈퇴</span>
+          <p>~ 님 탈퇴 하시겠습니까?</p>
+        </header>
+        <section className="resign_btn_wraps">
+          <button className="u_resign_btn">탈퇴</button>
+          <button onClick={closeModal} className="u_cancle_btn">
+            취소
+          </button>
+        </section>
+      </div>
     </div>
   );
 };
--- src/pages/user/user_update.tsx
+++ src/pages/user/user_update.tsx
@@ -1,12 +1,20 @@
-
-
+import User_resign from "../modal/user_resign";
+import { useDispatch } from "react-redux";
+import { set } from "../../reducers/modal/modalReducer";
+import "../../assets/styles/css/user/user_update.scss";
 const User_update: React.FC = () => {
+  const dispatch = useDispatch();
 
-    return (
-        <div>
-            <p>회원정보 수정페이지</p>
-        </div>
-        );
-}
+  const open_user_resign_modal = () => {
+    dispatch(set({ state: "유저탈퇴", cashe1: "캐시1", cashe2: "캐시2" }));
+  };
+  return (
+    <div className="user_update">
+      <p>회원정보 수정페이지</p>
+      <button onClick={open_user_resign_modal}>탈퇴</button>
+      <User_resign />
+    </div>
+  );
+};
 
 export default User_update;
