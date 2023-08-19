d70a64240548f7c8c8c7a60bac229e1d54b4c747
ksh <sehun910124@naver.com>
2023-08-19 14:41:25 +0900

    review_delete 모달

--- src/pages/modal/review_delete.tsx
+++ src/pages/modal/review_delete.tsx
@@ -1,16 +1,31 @@
 import "../../assets/styles/css/modal/review_delete.scss";
-
+import { set } from "../../reducers/modal/modalReducer";
+import { useDispatch } from "react-redux";
+import { RootState } from "../../reducers";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 const Review_delete: React.FC = () => {
+  const modalstate = useSelector((state: RootState) => state.modalReducer);
+  const dispatch = useDispatch();
+  const closeModal = () => {
+    dispatch(set({ ...modalstate, state: "" }));
+  };
+
+  if (modalstate.state === "") return null;
+
   return (
-    <div className="review_delete">
-      <header className="review_delete_tit">
-        <span>리뷰 삭제</span>
-        <p>리뷰를 삭제하시겠습니까?</p>
-      </header>
-      <section className="review_delete_btn_wraps">
-        <button className="r_delete_btn">삭제</button>
-        <button className="r_cancle_btn">취소</button>
-      </section>
+    <div className="modal_r_delete">
+      <div className="review_delete">
+        <header className="review_delete_tit">
+          <span>리뷰 삭제</span>
+          <p>리뷰를 삭제하시겠습니까?</p>
+        </header>
+        <section className="review_delete_btn_wraps">
+          <button className="r_delete_btn">삭제</button>
+          <button onClick={closeModal} className="r_cancle_btn">
+            취소
+          </button>
+        </section>
+      </div>
     </div>
   );
 };
